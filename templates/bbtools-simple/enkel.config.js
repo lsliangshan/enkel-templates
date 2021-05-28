d926752ca9e3585d5db1b360fb9773bf049736f7
liangshan <liang.shan@zhaopin.com.cn>
2021-05-28 15:51:34 +0800

    添加模板 bbtools-simple

---
+++ templates/bbtools-simple/enkel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  description: 'BBTools Simple',
+  placeholder: {
+    $$pluginName$$: {
+      description: '插件名称'
+    },
+    $$author$$: {
+      description: '作者'
+    },
+    $$homepage$$: {
+      description: '插件主页'
+    },
+    $$keyword$$: {
+      description: '搜索关键词'
+    }
+  }
+}
