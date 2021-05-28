d81a4f18810b1715cc6bf5c1b7eb3773c48e1474
liangshan <liang.shan@zhaopin.com.cn>
2021-05-28 16:55:23 +0800

    no message

--- templates/bbtools-simple/enkel.config.js
+++ templates/bbtools-simple/enkel.config.js
@@ -2,19 +2,24 @@ module.exports = {
   description: 'BBTools Simple',
   placeholder: {
     $$pluginName$$: {
-      description: '插件名称'
+      description: '插件名称',
+      type: 'String'
     },
     $$description$$: {
-      description: '插件描述'
+      description: '插件描述',
+      type: 'String'
     },
     $$author$$: {
-      description: '作者'
+      description: '作者',
+      type: 'String'
     },
     $$homepage$$: {
-      description: '插件主页'
+      description: '插件主页',
+      type: 'String'
     },
     $$keyword$$: {
-      description: '搜索关键词'
+      description: '搜索关键词',
+      type: 'Array'
     }
   }
 }
